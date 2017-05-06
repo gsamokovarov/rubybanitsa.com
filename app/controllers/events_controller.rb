6479bf6a609bdd7e05cd1bd67ceb3c09d95071bb
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-05-06 11:07:28 +0300

    Prepare showing more detailed event view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
 class EventsController < ApplicationController
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def index
     @events = Event.order(time: :desc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'events#index'
+
+  resources :events, only: %i(show index)
 end
