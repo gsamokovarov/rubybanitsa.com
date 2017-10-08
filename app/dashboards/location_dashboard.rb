f38c90063948715a2e66f82b9e1d908414753799
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-10-08 14:32:29 +0300

    Fix the admin events show page

--- app/dashboards/location_dashboard.rb
+++ app/dashboards/location_dashboard.rb
@@ -1,6 +1,43 @@
 require "administrate/base_dashboard"
 
 class LocationDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    venue: Field::BelongsTo,
+    event: Field::Number,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :venue,
+    :event,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :venue,
+    :event,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :venue,
+    :event,
+  ].freeze
+
   def display_resource(location)
     location.name
   end
