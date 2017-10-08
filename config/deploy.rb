b6dd2bc47ad47d096c32701a442d0d639f811a28
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-10-08 13:20:54 +0300

    Update the Capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-lock '3.8.1'
+lock '3.9.1'
 
 set :application, 'banitsa'
 set :repo_url, 'git@github.com:gsamokovarov/rubybanitsa.com.git'
 set :deploy_to, '/banitsa'
 set :ssh_options, forward_agent: true
+
+set :bundle_flags, nil
+set :bundle_without, nil
