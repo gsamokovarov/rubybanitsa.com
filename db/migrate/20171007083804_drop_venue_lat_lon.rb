cf4360d15b452eab27e44c25533233b9ece1e494
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-10-08 14:25:21 +0300

    Prevent "Cannot add a NOT NULL column with default value NULL"

--- db/migrate/20171007083804_drop_venue_lat_lon.rb
+++ db/migrate/20171007083804_drop_venue_lat_lon.rb
@@ -3,6 +3,6 @@ class DropVenueLatLon < ActiveRecord::Migration[5.1]
     remove_column :venues, :latitude, :float, default: 0.0, null: false
     remove_column :venues, :longitude, :float, default: 0.0, null: false
 
-    add_column :venues, :place_id, :string, null: false
+    add_column :venues, :place_id, :string, default: "", null: false
   end
 end
