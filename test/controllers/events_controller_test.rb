b4f6a4296bd6477eaec43f3d1f5d1ec5e8d42c2f
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-12-09 13:08:25 +0200

    Test the events controller

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'GET /events works without events' do
+    get events_path
+
+    assert_response :success
+    assert_select 'section#events', ''
+  end
+
+  test 'GET /events shows the recent events' do
+    travel_to Time.new(2015, 3, 19) do
+      venue = Venue.create!(name: 'N-working', address: 'Somewhere rue', place_id: 'foo')
+      event = Event.create_with_venue(time: Time.current,
+                                      description: 'Impulsive event',
+                                      venue_id: venue.id)
+
+      get events_path
+
+      assert_response :success
+      assert_select 'section#events', "Thursday, 19th of March in N-working \n\n  " \
+                                      "Impulsive event"
+    end
+  end
 end
