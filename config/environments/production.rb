e0cfb591099354fcbbc0e6569f436845b0054d67
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-12-20 12:57:59 +0200

    Don't show the timestamp during production logging
    We have other tools to annotate the time.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # Suppress the timestamp and the log level in the logs.
+  config.log_formatter = ActiveSupport::Logger::SimpleFormatter.new
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
