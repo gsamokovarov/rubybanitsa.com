642ccc5871dd286e48871fcfc024686dfbea42bf
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-05-10 00:33:28 +0300

    Revert "Fix and update early invocation"
    This reverts commit 0a9223d014b38e266e6aa02c8e489c3d21536e3d.

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require_relative 'early' # Default the environment variables.
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
