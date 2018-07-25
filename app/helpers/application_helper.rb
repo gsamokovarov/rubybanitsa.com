468163ca8ead4d3f3e54e69b6c3be0dadda67a90
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-07-25 13:41:44 +0300

    Extract the directional helper in own helper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def left(name, path)
-    content_for :left do
-      link_to "← #{name}", path
-    end
-  end
-
-  def right(name, path)
-    content_for :right do
-      link_to "#{name} →", path, class: :right
-    end
-  end
 end
