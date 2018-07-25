61c6e0b26d5a2e3c8f2479e25e9f6185341a948c
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-07-25 11:30:04 +0300

    Simplify directional menu setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def left(name, path)
+    content_for :left do
+      link_to "← #{name}", path
+    end
+  end
+
+  def right(name, path)
+    content_for :right do
+      link_to "#{name} →", path, class: :right
+    end
+  end
 end
