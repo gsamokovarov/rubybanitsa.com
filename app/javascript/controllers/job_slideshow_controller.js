3ec94395cc47744630f00184fca741c32c2b08bb
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-09-17 22:46:26 +0300

    Implement jerky autoscroll in the slides

--- app/javascript/controllers/job_slideshow_controller.js
+++ app/javascript/controllers/job_slideshow_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "stimulus"
 
 const SLIDE_SHOW_TIME = 60 * 1000 // 60 seconds
-const SCROLL_BY_PIXELS = 10
+const SCROLL_BY_PIXELS = 1
 
 export default class extends Controller {
   static targets = [ "slide" ]
 
   initialize() {
+    this.direction = 1
     this.showCurrentSlide()
   }
 
@@ -29,6 +30,31 @@ export default class extends Controller {
     this.slideTargets.forEach((el, i) => {
       el.classList.toggle("slide--current", this.index == i)
     })
+
+    this.scrollCurrentSlide()
+  }
+
+  scrollCurrentSlide() {
+    clearInterval(this.scrollInterval)
+
+    this.scrollInterval = setInterval(() => {
+      scrollBy(0, this.scrollOffset, {
+        behavior: "smooth"
+      })
+    }, 100)
+  }
+
+  reverseDirection() {
+    this.direction *= -1
+  }
+
+  get scrollOffset() {
+    const body = document.body
+
+    if (scrollY == 0) this.reverseDirection()
+    if (scrollY + innerHeight == body.scrollHeight) this.reverseDirection()
+
+    return this.direction * SCROLL_BY_PIXELS
   }
 
   get index() {
