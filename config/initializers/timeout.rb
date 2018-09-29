d3d5164c0aa7ee819e9b192362e6e0467ce2c8b7
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-09-29 21:05:46 +0300

    Respect rack-timeout 0.5.1 configuration

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 if defined?(Rack::Timeout)
-  Rack::Timeout.timeout = Integer(25.seconds)
+  Rails.configuration.middleware.insert_before Rack::Runtime, Rack::Timeout,
+    service_timeout: Integer(25.seconds)
 end
