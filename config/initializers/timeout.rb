933f213fba3a5e4a0490b8d98e81f0c65724b785
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-09-30 09:31:24 +0300

    Introduce a dynamic rack-timeout configuration through ENVs
    Don't hard-code the timeout, so it can be tweaked from Heroku's admin
    console.

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 if defined?(Rack::Timeout)
-  Rails.configuration.middleware.insert_before Rack::Runtime, Rack::Timeout,
-    service_timeout: Integer(25.seconds)
+  Rails.configuration.middleware.insert_before Rack::Runtime, Rack::Timeout
 end
