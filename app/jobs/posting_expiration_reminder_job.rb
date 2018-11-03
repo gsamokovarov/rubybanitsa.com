42717fc1ab955618d42a2ba5f51f540c8e6c4e4a
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-11-03 14:47:29 +0200

    Introduce a job posting expiration reminder sending job

---
+++ app/jobs/posting_expiration_reminder_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class PostingExpirationReminderJob < ApplicationJob
+  DAYS_TO_REMIND_ON = [3, 1]
+
+  def perform(job)
+    return if DAYS_TO_REMIND_ON.none? { |day| expires_in_days?(job, day) }
+
+    job.remind
+  end
+
+  private
+
+  def expires_in_days?(job, n)
+    job.expires_at.to_date == n.days.from_now.to_date
+  end
+end
