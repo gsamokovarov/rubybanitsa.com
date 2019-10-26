1c37125a81739e6846c43817acb7a22bdbc352a3
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-10-26 20:21:41 +0300

    Show the active jobs on event pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,12 @@ class EventsController < ApplicationController
   include TimeHelper
 
   def show
+    @jobs = Job.current
     @event = Event.find(params[:id])
   end
 
   def index
+    @jobs = Job.current
     @events = Event.during(during)
   end
 end
