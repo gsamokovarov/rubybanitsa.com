ed4e78c2daa074400e5f73e407d16636930a2812
Genadi Samokovarov <gsamokovarov@gmail.com>
2021-03-16 09:17:19 +0200

    Fix the calendar CN values
    They need to be emails.

--- app/models/event_calendar.rb
+++ app/models/event_calendar.rb
@@ -3,9 +3,7 @@
 module EventCalendar
   extend self
 
-  ORGANIZER = "Genadi Samokovarov"
-  ORGANIZER_EMAIL = "mailto:genadi@hey.com"
-
+  ORGANIZER_EMAIL = "genadi@hey.com"
   EVENT_DURATION = 1.hour
 
   def for(event)
@@ -17,8 +15,8 @@ module EventCalendar
 
       e.dtstart = Icalendar::Values::DateTime.new(event.time, tzid: tzid)
       e.dtend = Icalendar::Values::DateTime.new(event.time + EVENT_DURATION, tzid: tzid)
-      e.organizer = Icalendar::Values::CalAddress.new(ORGANIZER_EMAIL, cn: ORGANIZER)
-      e.attendee = Icalendar::Values::CalAddress.new(ORGANIZER_EMAIL, cn: ORGANIZER)
+      e.organizer = Icalendar::Values::CalAddress.new("mailto:#{ORGANIZER_EMAIL}", cn: ORGANIZER_EMAIL)
+      e.attendee = Icalendar::Values::CalAddress.new("mailto:#{ORGANIZER_EMAIL}", cn: ORGANIZER_EMAIL)
       e.url = Link.event_url(event)
       e.summary = info.calendar_title
       e.description = MD.render_plain(info.calendar_description)
