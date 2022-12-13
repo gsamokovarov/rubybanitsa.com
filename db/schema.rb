3b4189c6a5483df16d0ba6509fe7bad704a82a51
Genadi Samokovarov <gsamokovarov@gmail.com>
2022-12-13 20:40:38 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_19_120848) do
+ActiveRecord::Schema.define(version: 2022_12_13_183905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_11_19_120848) do
     t.string "facebook_url", default: "", null: false
     t.datetime "meetup_published_at"
     t.string "online_url", default: ""
+    t.string "name"
   end
 
   create_table "jobs", force: :cascade do |t|
