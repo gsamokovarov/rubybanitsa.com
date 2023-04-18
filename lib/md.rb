5c7a5cdb66bcab586e153c2a4a6577a5990b81cc
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-04-18 16:45:50 +0300

    Define PlainTextRenderer before its usage
    Whoops, and it twerks!?

--- lib/md.rb
+++ lib/md.rb
@@ -5,6 +5,16 @@ require "redcarpet/render_strip"
 # MD is a facade over the Redcarpet markdown rendering library with preset HTML
 # and Plain text renderers.
 module MD
+  class PlainTextRenderer < Redcarpet::Render::StripDown
+    def paragraph(text)
+      "#{text}\n\n"
+    end
+
+    def header(text, _header_level)
+      "#{text}\n\n"
+    end
+  end
+
   cattr_accessor :html do
     Redcarpet::Markdown.new Redcarpet::Render::HTML,
                             autolink: true,
@@ -24,15 +34,5 @@ module MD
 
   def render_plain(content)
     plain.render(content)
-  end
-
-  class PlainTextRenderer < Redcarpet::Render::StripDown
-    def paragraph(text)
-      "#{text}\n\n"
-    end
-
-    def header(text, _header_level)
-      "#{text}\n\n"
-    end
   end
 end
