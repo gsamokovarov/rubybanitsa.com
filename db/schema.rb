4e565585cabd9dce0430da6a1644556388008e64
Ventsislav Nikolov <vencislaf@gmail.com>
2023-06-30 17:34:31 +0300

    Add NOT NULL constraint to events.venue_id
    Once we ensure all events in the databaase are associated with venues,
    we can improve the data integrity by making the `events.venue_id` column
    not nullable.
    
    The migration is done in a naive way since we have around 50 events at
    the moment and blocking the table shouldn't be a problem.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_30_124814) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_143115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_124814) do
     t.datetime "meetup_published_at", precision: nil
     t.string "online_url", default: ""
     t.string "name"
-    t.bigint "venue_id"
+    t.bigint "venue_id", null: false
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
