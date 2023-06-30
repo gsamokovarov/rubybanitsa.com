03236787781ba137754de8758dfee18f7a970359
Ventsislav Nikolov <vencislaf@gmail.com>
2023-06-30 12:42:55 +0300

    Clean up event publishing
    Fix event publishing by removing the call to the deleted
    `MailinglistPublisher` service. Also, simplify the event publishing
    by deleting jobs and tests that are not necessary anymore.

--- app/jobs/event_publishing_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class EventPublishingJob < ApplicationJob
-  def perform(event)
-    event.publish do
-      MailinglistPublisherJob.perform_later(event)
-    end
-  end
-end
