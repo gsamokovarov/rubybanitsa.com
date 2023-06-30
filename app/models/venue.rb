472f45e5151f9f442ccf51dc327652f5d056b613
Ventsislav Nikolov <vencislaf@gmail.com>
2023-06-30 18:10:26 +0300

    Move has_many statement before validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Venue < ApplicationRecord
+  has_many :events
+
   validates :name, presence: true
   validates :address, presence: true, unless: :online?
   validates :place_id, presence: true, unless: :online?
-
-  has_many :events
 end
