f49853ef9936a668adbee900b823c8110bfa934e
Ventsislav Nikolov <vencislaf@gmail.com>
2023-10-03 12:19:30 +0300

    Embed talk youtube video on event details page

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -5,4 +5,21 @@ class Talk < ApplicationRecord
   has_and_belongs_to_many :speakers
 
   validates :title, presence: true
+
+  def embedded_youtube_url
+    return nil if url.blank?
+
+    uri = URI(url)
+
+    video_id =
+      if uri.host&.include?("youtube.com")
+        URI.decode_www_form(uri.query).to_h["v"]
+      elsif uri.host&.include?("youtu.be")
+        uri.path.remove("/")
+      end
+
+    return nil unless video_id
+
+    "https://www.youtube.com/embed/#{video_id}"
+  end
 end
