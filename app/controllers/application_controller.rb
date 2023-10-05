0d995b71692e4aad005c8c348625b3263f340de9
Ventsislav Nikolov <vencislaf@gmail.com>
2023-10-05 22:14:11 +0200

    Move upcoming event out from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,9 @@
 class ApplicationController < ActionController::Base
   include Administration
 
-  before_action :set_upcoming_event
   before_action :set_current_jobs
 
   private
-
-  def set_upcoming_event
-    @upcoming_event = Event.upcoming
-  end
 
   def set_current_jobs
     @current_jobs = Job.current
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 
 class HomeController < ApplicationController
   def show
+    @upcoming_event = Event.upcoming
     @recent_events = Event.recent(3)
   end
 end
