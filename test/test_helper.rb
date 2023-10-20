810b8bea4be4dc1c2bda9453c476862e59ef9944
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-10-20 14:37:00 +0300

    Update to Rails 7.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -49,7 +49,7 @@ end
 module ControllerCatchAllRoutes
   Router = ActionDispatch::Routing::RouteSet.new
   Router.draw do
-    ActiveSupport::Deprecation.silence { get ":controller(/:action)" }
+    Rails.application.deprecators.silence { get ":controller(/:action)" }
   end
 
   def before_setup
